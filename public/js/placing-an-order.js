2db084108a1eb38e6de7415285b82a1fd625144f
Seeminglone <yurakravtsov833@gmail.com>
2023-06-09 21:47:19 +0200

    Edit order-01

--- public/js/placing-an-order.js
+++ public/js/placing-an-order.js
@@ -166,7 +166,11 @@ function toggleOptionsVisibility(event, containerId) {
   let arrowDownClick = document.querySelector('.arrow-down')
   let selectInputHouseShow = document.querySelector('.select-input-house');
   let arrowDownClickHouse = document.querySelector('.arrow-down-house');
-  
+  let wrapperContainerFirstShow = document.querySelector('.regions-container-first');
+  let wrapperContainerHouseFirstShow = document.querySelector('.regions-container-house-first');
+
+ 
+
   if (optionsContainer && inputElement) {
     if (optionsContainer.style.display === 'none') {
       optionsContainer.style.display = 'block';
@@ -177,6 +181,8 @@ function toggleOptionsVisibility(event, containerId) {
       arrowDownClick.classList.add('arrow-down-click')
       selectInputHouseShow.classList.add('select-input-house-show')
       arrowDownClickHouse.classList.add('arrow-down-house-click')
+      wrapperContainerFirstShow.classList.add('regions-container-first-show')
+      wrapperContainerHouseFirstShow.classList.add('regions-container-house-first-show')
 
       optionsContainer.style.maxHeight = '120px';
       optionsContainer.style.overflowY = 'auto';
@@ -189,6 +195,8 @@ function toggleOptionsVisibility(event, containerId) {
       wrapperShow.classList.remove('select-wrapper-container-show')
       selectInputHouseShow.classList.remove('select-input-house-show')
       arrowDownClickHouse.classList.remove('arrow-down-house-click')
+      wrapperContainerFirstShow.classList.remove('regions-container-first-show')
+      wrapperContainerHouseFirstShow.classList.remove('regions-container-house-first-show')
     }
 
     if (containerId !== 'regions') {
@@ -199,6 +207,7 @@ function toggleOptionsVisibility(event, containerId) {
       wrapperShow.classList.remove('select-wrapper-container-show')
       selectInputShow.classList.remove('select-input-show')
       arrowDownClick.classList.remove('arrow-down-click')
+      wrapperContainerFirstShow.classList.remove('regions-container-first-show')
       
     }
     if (containerId !== 'postOffices') {
@@ -209,6 +218,7 @@ function toggleOptionsVisibility(event, containerId) {
       wrapperShowHouse.classList.remove('select-wrapper-container-house-show')
       selectInputHouseShow.classList.remove('select-input-house-show')
       arrowDownClickHouse.classList.remove('arrow-down-house-click')
+      wrapperContainerHouseFirstShow.classList.remove('regions-container-house-first-show')
     }
   }
 
@@ -284,12 +294,17 @@ document.addEventListener('click', function () {
   let wrapperShowHouse = document.querySelector('.select-wrapper-container-house')
   let selectInputHouseShow = document.querySelector('.select-input-house');
   let arrowDownClickHouse = document.querySelector('.arrow-down-house');
+  let wrapperContainerFirstShow = document.querySelector('.regions-container-first');
+  let wrapperContainerHouseFirstShow = document.querySelector('.regions-container-house-first');
+
+  wrapperContainerHouseFirstShow.classList.remove('regions-container-house-first-show')
   wrapperShow.classList.remove('select-wrapper-container-show')
   selectInputShow.classList.remove('select-input-show')
   arrowDownClick.classList.remove('arrow-down-click')
   wrapperShowHouse.classList.remove('select-wrapper-container-house-show')
   selectInputHouseShow.classList.remove('select-input-house-show')
   arrowDownClickHouse.classList.remove('arrow-down-house-click')
+  wrapperContainerFirstShow.classList.remove('regions-container-first-show')
   const postOfficesOptionsContainer = document.getElementById('postOfficesOptions');
   postOfficesOptionsContainer.style.display = 'none';
 
@@ -302,3 +317,358 @@ document.addEventListener('click', function () {
 });
 
 fetchRegions();
+
+let main = document.querySelector(".kek");
+let cartProductsPage = document.querySelector(".cart__products-product");
+var x = localStorage.getItem("numberLS");
+let arr = [];
+
+arr = x.split(",");
+let displayedElements = [];
+let quantityCount = {};
+arr.forEach((e) => {
+  if (quantityCount[e]) {
+    quantityCount[e]++;
+  } else {
+    quantityCount[e] = 1;
+  }
+});
+
+arr.forEach((e) => {
+  const sizeMap = new Map();
+  sizeMap.set("ONE_AND_HALF", "Півтораспальний");
+  sizeMap.set("DOUBLE", "Двохспальний");
+  sizeMap.set("EURO", "Євро");
+  sizeMap.set("FAMILY", "Сімейний");
+
+  const materialMap = new Map();
+  materialMap.set("RANFORS", "Ранфорс");
+  materialMap.set("SATIN", "Сатин");
+  materialMap.set("STRAP_SATIN", "Страйп сатин");
+  materialMap.set("POPLIN", "Поплин");
+  materialMap.set("BIAZ", "Бязь");
+
+  const colorMap = new Map();
+  colorMap.set("RED", "Червоний");
+  colorMap.set("ORANGE", "Помаранчовий");
+  colorMap.set("YELLOW", "Жовтий");
+  colorMap.set("GREEN", "Зелений");
+  colorMap.set("BLUE", "Блакитний");
+  colorMap.set("INDIGO", "Індіго");
+  colorMap.set("VIOLET", "Фіолетовий");
+  colorMap.set("PINK", "Рожевий");
+  colorMap.set("SILVER", "Сріблястий");
+  colorMap.set("GOLD", "Золотий");
+  colorMap.set("BEIGE", "Бежевий");
+  colorMap.set("BROWN", "Коричневий");
+  colorMap.set("GRAY", "Сірий");
+  colorMap.set("BLACK", "Чорний");
+  colorMap.set("WHITE", "Білий");
+
+  fetch(`${serverMachineUrl}/api/textile?id=${e}`, {
+    method: "GET",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      const storedQuantity = localStorage.getItem("numberLS");
+
+      if (storedQuantity === "") {
+        cartProductsPage.classList.add("cart__products-product-unshow");
+        cartProductsPage.innerHTML = `<h2 class="title-cart-clear">В кошику не має товарів!</h2>
+        <a type="button" href="index.html" class="back-from-cart-to-catalog">Повернутися до покупок</a>`;
+      } else {
+        cartProductsPage.classList.remove("cart__products-product-unshow");
+        if (!displayedElements.includes(json.id)) {
+          const quantity = quantityCount[json.id];
+          const storedQuantity = localStorage.getItem("numberLS");
+          const storedQuantityArray = storedQuantity
+            ? storedQuantity.split(",")
+            : [];
+          const storedQuantityCount = storedQuantityArray.filter(
+            (id) => id === json.id
+          ).length;
+          formattedDiscountPrice = json.discountPrice.toLocaleString("uk-UA");
+          cartProductsPage.innerHTML += `<div class="products-container">
+                                          <div class="img-cart">
+                                          <img src="${json.imgUrl}">
+                                        </div>
+                                        <span class="cart-name">${
+                                          json.name
+                                        }</span>
+                                        <table class="cart__config">
+                                          <tbody><tr>
+                                              <th class="size-cart">Розмір:</th>
+                                              <td>${sizeMap.get(json.size)}</td>
+                                          </tr>
+                                              <th class="color-cart">Колір:</th>
+                                              <td>${colorMap.get(
+                                                json.color
+                                              )}</td>
+                                          </tr>
+                                          </tr>
+                                              <th class="material-cart">Матеріал:</th>
+                                              <td>${materialMap.get(
+                                                json.material
+                                              )}</td>
+                                          </tr>
+                                          </tbody>
+                                        </table>
+                                        <span type="number" class="cart-price" data-id="${
+                                          json.id
+                                        }" value="1">${formattedDiscountPrice} грн</span>
+                                        <div class="cart__counter">
+                                          <span>Кількість:</span>
+                                          <input type="text" readonly name="quantity" сlass="input-quantity" data-id="${
+                                            json.id
+                                          }" value="${quantity}">
+                                          <div class="arrow-container">
+                                            <img type="button" src="images/increase.png" class="increase" data-id="${
+                                              json.id
+                                            }"></img>
+                                            <img class="decrease" type="button" src="images/increase.png" data-id="${
+                                              json.id
+                                            }"></img>
+                                          </div>
+                                        </div>
+                                        <span class="cart-price-all" data-id="${
+                                          json.id
+                                        }" value="0" id="cart-price-all-${
+            json.id
+          }" data-price="${json.discountPrice * quantity}" style="
+          display: none;
+      "></span>
+                                        <button data-id="${
+                                          json.id
+                                        }" class="cart-delete-btn"><i class="fas fa-trash-alt" style="color: black; transition: all 0.2s linear; font-size: 18px;font-weight: 100;" 
+                                        onmouseover="this.style.color='#EA4C89';" onmouseout="this.style.color='black';"></i></button>
+                                      </div> `;
+          let cartSummaryContainer = document.querySelector(
+            ".cart-summary-container"
+          );
+          cartSummaryContainer.innerHTML = `<div class="cart-summary">
+                                                                          <h2 class="cart-summaty-title">Підсумок замовлення:</h2>
+                                                                          
+                                                                        </div>`;
+          let totalSum = 0;
+
+          displayedElements.push(json.id);
+          let decreaseButtons = document.querySelectorAll(".decrease");
+          let increaseButtons = document.querySelectorAll(".increase");
+          let deleteBtn = document.querySelectorAll(".cart-delete-btn");
+          let quantityInputs = document.querySelectorAll(
+            'input[name="quantity"]'
+          );
+          const button = document.querySelector(`button[data-id="${json.id}"]`);
+          const parentElement = button.parentNode;
+          const newPriceElement = document.createElement("span");
+          const price = json.discountPrice * quantity;
+          const formattedPrice = formatPrice(price);
+          newPriceElement.innerText = `${formattedPrice} грн`;
+          parentElement.insertBefore(newPriceElement, button);
+
+          let cartPriceElements = document.querySelectorAll(".cart-price");
+          let cartPriceAllElements =
+            document.querySelectorAll(".cart-price-all");
+          cartPriceAllElements.forEach((element) => {
+            const price = parseFloat(element.getAttribute("data-price"));
+            let formattedPrice = formatPrice(price);
+            totalSum += price;
+          });
+
+          function formatPrice(price) {
+            return price.toLocaleString("uk-UA");
+          }
+
+          let formattedTotalSum = totalSum.toFixed(2);
+          if (formattedTotalSum.endsWith(".00")) {
+            formattedTotalSum = formattedTotalSum.replace(".00", "");
+          }
+          formattedTotalSum = totalSum.toLocaleString("uk-UA");
+          const cartSummaryTitle = document.querySelector(
+            ".cart-summaty-title"
+          );
+          cartSummaryTitle.innerText = `Загальна вартість: ${formattedTotalSum} грн`;
+
+          deleteBtn.forEach((button) => {
+            button.addEventListener("click", () => {
+              const container = button.closest(".products-container");
+              const id = container
+                .querySelector(".cart-price-all")
+                .getAttribute("data-id");
+              container.remove();
+              const storedQuantity = localStorage.getItem("numberLS");
+              const storedQuantityArray = storedQuantity
+                ? storedQuantity.split(",")
+                : [];
+              const updatedQuantityArray = storedQuantityArray.filter(
+                (itemId) => itemId !== id
+              );
+              localStorage.setItem("numberLS", updatedQuantityArray.join(","));
+
+              location.reload();
+            });
+          });
+
+          increaseButtons.forEach((button) => {
+            button.addEventListener("click", () => {
+              let id = button.getAttribute("data-id");
+              let quantityInput = document.querySelector(
+                `input[data-id="${id}"]`
+              );
+              let quantity = parseInt(quantityInput.value);
+              let arrayBagage = localStorage.getItem("numberLS")
+                ? localStorage.getItem("numberLS").split(",")
+                : [];
+              let count = arrayBagage.length;
+              counterBagadge.innerHTML = ++count;
+              let storedQuantity = localStorage.getItem("numberLS");
+              let storedQuantityArray = storedQuantity
+                ? storedQuantity.split(",")
+                : [];
+              if (
+                storedQuantityArray.includes(id) &&
+                !quantityCount[id].increased
+              ) {
+                quantity += 1;
+                quantityCount[id].increased = true;
+              } else {
+                quantity = 1;
+              }
+              quantityInput.value = quantity;
+              updateCartPrice(id, quantity);
+              addToLocalStorage(id, quantity);
+              location.reload();
+            });
+          });
+
+          decreaseButtons.forEach((button) => {
+            button.addEventListener("click", () => {
+              let id = button.getAttribute("data-id");
+              let quantityInput = document.querySelector(
+                `input[data-id="${id}"]`
+              );
+              let quantity = parseInt(quantityInput.value);
+
+              if (quantity > 1) {
+                let arrayBagage = localStorage.getItem("numberLS")
+                  ? localStorage.getItem("numberLS").split(",")
+                  : [];
+                let count = arrayBagage.length;
+                counterBagadge.innerHTML = --count;
+                quantity--;
+                quantityInput.value = quantity;
+                updateCartPrice(id, quantity);
+                removeFromLocalStorage(id);
+                quantityCount[id] = quantity;
+                location.reload();
+              }
+            });
+          });
+
+          const quantityInputse = document.querySelectorAll(
+            'input[name="quantity"]'
+          );
+
+
+          function updateCartPrice(id, quantity) {
+            const price = parseFloat(prices[id]);
+            const cartPriceElement = document.querySelector(
+              `.cart-price[data-id="${id}"]`
+            );
+            const cartPriceAllElement = document.querySelector(
+              `.cart-price-all[data-id="${id}"]`
+            );
+            const totalPrice = price * quantity;
+
+            cartPriceElement.innerText = price.toFixed(2);
+            cartPriceAllElement.innerText = totalPrice.toFixed(2);
+          }
+
+          function addToLocalStorage(id) {
+            let storedQuantity = localStorage.getItem("numberLS");
+
+            if (storedQuantity) {
+              let updatedQuantity = `${storedQuantity},${id}`;
+              localStorage.setItem("numberLS", updatedQuantity);
+            } else {
+              localStorage.setItem("numberLS", id);
+            }
+          }
+
+          function removeFromLocalStorage(id) {
+            let storedQuantity = localStorage.getItem("numberLS");
+
+            if (storedQuantity) {
+              let quantityArray = storedQuantity.split(",");
+              let index = quantityArray.lastIndexOf(id);
+
+              if (index !== -1) {
+                quantityArray.splice(index, 1);
+                let updatedQuantity = quantityArray.join(",");
+                localStorage.setItem("numberLS", updatedQuantity);
+              }
+            }
+          }
+
+          function updateCartPrice(id, quantity) {
+            let priceElement = document.querySelector(
+              `.cart-price[data-id="${id}"]`
+            );
+            let cartPriceAllElement = document.querySelector(
+              `.cart-price-all[data-id="${id}"]`
+            );
+
+            if (cartPriceAllElement) {
+              let price = parseFloat(priceElement.innerText);
+              let totalPrice = price * quantity;
+
+              cartPriceAllElement.innerHTML =
+                totalPrice % 1 === 0
+                  ? totalPrice.toFixed(0)
+                  : totalPrice.toFixed(2);
+            } else {
+              console.error(`Element with data-id "${id}" not found.`);
+            }
+          }
+        }
+      }
+    });
+});
+
+ // Знаходимо елементи radio button
+ const bacsRadio = document.querySelector('.full-paymant-container');
+ const codRadio = document.querySelector('.avans-paymant-container');
+ let fullPaymentBorder = document.querySelector('.full-paymant-container');
+ let fullPaymentTitle = document.querySelector('.full-paymant-title-label');
+ let avansPaymentTitle = document.querySelector('.avams-paymant-title-label');
+ let avansPaymantBorder = document.querySelector('.avans-paymant-container');
+ avansPaymantBorder.classList.remove('avans-paymant-container')
+ avansPaymentTitle.classList.remove('avams-paymant-title-label')
+ // Знаходимо елементи з вмістом параграфів
+ const bacsParagraph = document.querySelector('.payment_method_bacs p');
+ const codParagraph = document.querySelector('.payment_method_cod p');
+ let codParagraphShow = document.querySelector('.kek')
+ // Встановлюємо обробники подій на клік
+ bacsRadio.addEventListener('click', function() {
+  fullPaymentBorder.classList.add('full-paymant-container')
+  fullPaymentTitle.classList.add('full-paymant-title-label')
+  avansPaymentTitle.classList.remove('avams-paymant-title-label')
+  avansPaymantBorder.classList.remove('avans-paymant-container')
+  codParagraph.innerHTML = '' 
+  bacsParagraph.innerHTML = 'Оплата повним платежем. Оплату приймаємо на карту (відправимо реквізити в viber/telegram).';
+ });
+
+ codRadio.addEventListener('click', function() {
+  avansPaymantBorder.classList.add('avans-paymant-container')
+  fullPaymentTitle.classList.remove('full-paymant-title-label')
+  fullPaymentBorder.classList.remove('full-paymant-container')
+  codParagraphShow.classList.add('kek-show')
+  avansPaymentTitle.classList.add('avams-paymant-title-label')
+  bacsParagraph.innerHTML = ''
+  codParagraph.innerHTML = 'Оплата накладним платежем при отриманні. Аванс на відправку накладним платежем 150 грн на карту (відправимо реквізити в viber/telegram).';
+ });
