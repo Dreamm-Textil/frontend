a954cf3fd00b96d2683476a260a9eccd8be62170
Seeminglone <yurakravtsov833@gmail.com>
2023-08-26 11:07:20 +0200

    font-family-edit

--- public/js/server-machine-url.js
+++ public/js/server-machine-url.js
@@ -1,4 +1,4 @@
-let serverMachineUrl = 'https://ec2-54-90-234-207.compute-1.amazonaws.com:8080';
+let serverMachineUrl = 'http://ec2-54-90-234-207.compute-1.amazonaws.com:8080';
 const authorizationCookieValue = getCookieValue('Authorization');
 
 
